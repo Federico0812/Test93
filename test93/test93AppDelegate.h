ca48365ef969b266327c114a608b1b5b38debc91
Federico0812 <federico0812@gmail.com>
2013-05-28 14:09:58 -0300

    Test
    ...

---
+++ test93/test93AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  test93AppDelegate.h
+//  test93
+//
+//  Created by fede on 11/01/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class test93ViewController;
+
+@interface test93AppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet test93ViewController *viewController;
+
+@end
---
+++ test93/test93ViewController.h
@@ -0,0 +1,13 @@
+//
+//  test93ViewController.h
+//  test93
+//
+//  Created by fede on 11/01/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test93ViewController : UIViewController
+
+@end
